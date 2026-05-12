9982e074eeab39215c374a5507114a41ca92cb07
joaquinpereira1993 <joaquinpereira40@gmail.com>
2026-05-11 23:02:26 -0300

    actualizacion de practicas de Java

--- src/tema04_expresiones_aritmetricas_y_funciones/ExpresionesAritméticasFuncionesM/ExpArimetricasMatematicas.java
+++ src/tema04_expresiones_aritmetricas_y_funciones/ExpresionesAritméticasFuncionesM/ExpArimetricasMatematicas.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ExpArimetricasMatematicas {
     public static void main(String[] args) {
-
+/*
         //1- Obtener el largo del rectangulo
         System.out.println("Ingrese el largo del rectangulo");
 
@@ -26,5 +26,32 @@ public class ExpArimetricasMatematicas {
        // 4- imprimir el resultado
         System.out.println("El area del rectangulo es: " + area);
 
+ */     //Funciones con Variables
+        int x, y;
+        x = 3;
+        int valor = x + 9;
+        System.out.println("El valor es: " + valor);
+
+        y = 29;
+        int variosOperadores = y + 4 + x + 10;
+        System.out.println("El resultado es: " + variosOperadores);
+
+        int ecuacion = y + x + 20 * 2;
+        System.out.println("El resultado es: " + ecuacion);
+
+        int parentesis = y + 3 - (x * 2);
+        System.out.println("El resultado es: " + parentesis);
+
+        double raizCuadrada = 4 - Math.sqrt(4);
+        System.out.println("La raiz cuadrada es: " + raizCuadrada);
+
+        int divisionEntera = 11 / 2;
+        System.out.println("El resultado de la division entera es: " + divisionEntera);
+
+        double divisionReal = 11.0 / 2.0;
+        System.out.println("El resultado de la division real es: " + divisionReal);
+
+        int resto = 11 % 2;
+        System.out.println("El resto es: " + resto);
     }
 }
